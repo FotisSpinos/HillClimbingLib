807e0ef0a6d0e6af7d8736321932553e88f83df2
Fotios_Sublime <fotios@sublime.cc>
2021-09-12 21:30:57 +0100

    One file hcl

--- Abstract/BaseHeuristic.ts
+++
@@ -1,8 +0,0 @@
-import { BaseHillClimbingState } from "./BaseHillClimbingState.js";
-
-export abstract class BaseHeuristic
-{
-    evaluate(state: BaseHillClimbingState): number{
-        throw new Error("Not Implemented method");
-    }
-}
